aaa7965ba83a6d4966ea05f00ecbb23ed905df88
Razvan <razvanbilici@yahoo.com>
2026-03-04 03:36:34 +0200

    Initial commit: Vue + Tailwind project scaffold

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from "./components/Navbar.vue"
+</script>
+
+<template>
+  <div class="min-h-screen bg-gray-300">
+
+    <Navbar />
+
+    <main class="p-6">
+      
+    </main>
+
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="flex items-center h-[10vh] max-w-full 
+              bg-linear-to-r from-gray-700 via-purple-800 to-gray-400 
+              rounded-b shadow-2xl px-4 text-[clamp(3rem,8vh,5rem)] overflow-hidden"> 
+                   
+    <div class="font-pcb ml-3 text-red-800 
+                hover:text-red-600 transition-all duration-200 
+                hover:scale-110 cursor-pointer text-outline">
+      Logo
+    </div>
+
+    <div class="flex flex-1 justify-end space-x-[4vw] text-outline font-pcb 
+                 text-white">
+      <a href="#" class="hover:scale-110 transition-transform">Songs</a>
+      <a href="#" class="hover:scale-110 transition-transform">Theory</a>
+      <a href="#" class="hover:scale-110 transition-transform">Progress</a>
+    </div>
+  </nav>
+</template>
