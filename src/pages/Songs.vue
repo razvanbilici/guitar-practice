f0b3f8bb967041ebec48dfa89dd5273e30c5c7fd
Razvan <razvanbilici@yahoo.com>
2026-03-04 05:00:14 +0200

    Mock embeded youtube videos test

--- src/pages/Songs.vue
+++ src/pages/Songs.vue
@@ -1,4 +1,40 @@
 <template>
 
-<div>Song page</div>
+
+  <div class="flex flex-row gap-4 p-4 justify-center">
+    <div class="flex flex-col gap-0.5 hover:cursor-pointer" v-for="folder in folders" v-on:click="open_close_folder(folder)">
+        {{ folder.name }}
+
+
+        <iframe v-if="folder.open" v-for="url in folder.urls"
+      width="320"
+      height="180"
+      :src="`https://www.youtube.com/embed/${url}`"
+      frameborder="0"
+      allowfullscreen
+    ></iframe>
+        
+    </div>
+  </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+
+const folders = ref([
+    {name: "Left",
+     urls: ["dQw4w9WgXcQ", "Gx1oCI9ZjZw"],
+     open: false
+    }, 
+    {name: "Right",
+     urls: ["cHYq1MRoyI0", ""],
+     open: false
+    }
+])
+
+function open_close_folder(folder){
+
+    folder.open = !folder.open
+    
+}
+</script>
